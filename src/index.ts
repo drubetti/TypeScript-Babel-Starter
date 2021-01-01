ef7c3754cfae35b077648427bc02f7dcb3d31a60
Daniele Rubetti <d.rubetti@palmabit.com>
2021-01-01 21:51:58 +0100

    Add babel-node, prettier, update dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,3 @@
-export class C {
-    private x = 10;
-    getX = () => this.x;
-    setX = (newVal: number) => { this.x = newVal; }
-}
+import 'dotenv/config';
 
-export let x = new C();
-export let y = { ...{ some: "value" } };
+console.log('It works!');
